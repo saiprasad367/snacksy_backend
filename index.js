1348b7bd27e0539a0f0912a1eb626c71bf0d5193
SaiPrasad <saiprasad2523@gmail.com>
2025-07-17 21:33:23 +0530

    index files has been changed

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ const path = require('path');
 
 
 const app = express();
+const PORT = process.env.PORT || 4000;
 dotenv.config();
+
 
 mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("Database connected successfully"))
@@ -22,12 +24,17 @@ app.use(bodyParser.json());
 app.use('/vendor', vendorRoutes);
 app.use('/firm', firmRoutes);
 app.use('/product',productroutes);
+app.use('/uploads',express.static('uploads'));
 
 
 app.get('/home', (req, res) => {
   res.send("<h1>Welcome</h1>");
 });
 
-app.listen(4000, () => {
+app.listen(PORT, () => {
   console.log("Server is running on port 4000");
+});
+
+app.use('/',(req,res)=>{
+  res.send("<h1> Welcome to Snacksy<</h1> ");
 });
